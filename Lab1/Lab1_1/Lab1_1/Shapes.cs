60826dac30dbefe49d65878d1bddd7c9460a0885
Aleksandr Kislitsyn <aleksandr.kislitsyn0@gmail.com>
2023-09-18 21:28:19 +0300

    init lab 1 1

---
+++ Lab1/Lab1_1/Lab1_1/Shapes.cs
@@ -0,0 +1,26 @@
+﻿namespace Lab1_1
+{
+    public class Shapes
+    {
+        private readonly TextReader _textReader;
+        private readonly TextWriter _textWriter;
+        public bool IsRun { get; private set; }
+
+        public Shapes(TextReader textReader, TextWriter textWriter)
+        {
+            _textReader = textReader;
+            _textWriter = textWriter;
+            IsRun = true;
+        }
+        
+        public void HandleInput()
+        {
+            
+        }
+        
+        private void Close()
+        {
+            IsRun = false;
+        }
+    }
+}
