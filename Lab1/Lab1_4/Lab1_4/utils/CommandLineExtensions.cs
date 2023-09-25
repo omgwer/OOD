62f1a09a0afe7fec6d6a30092dd2028b576d8a2b
Aleksandr Kislitsyn <aleksandr.kislitsyn0@gmail.com>
2023-09-25 20:55:46 +0300

    feat: Добавил начало лабы 1.4

---
+++ Lab1/Lab1_4/Lab1_4/utils/CommandLineExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab1_4.utils
+{
+    public static class CommandLineExtensions
+    {
+        public static IReadOnlyList<string> ToArgumentsList(this string inputString)
+        {
+            return inputString.Split( ' ' ).ToList();
+        }
+    }
+}
